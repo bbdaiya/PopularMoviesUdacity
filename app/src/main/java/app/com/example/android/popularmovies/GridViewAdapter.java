a22b9202a4a086d2036670ae24f9774106d525b3
Brijesh Daiya <bbdaiya@gmail.com>
2016-04-14 14:24:00 +0530

    First commit

---
+++ app/src/main/java/app/com/example/android/popularmovies/GridViewAdapter.java
@@ -0,0 +1,86 @@
+package app.com.example.android.popularmovies;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by bbdaiya on 7/4/16.
+ */
+public class GridViewAdapter extends ArrayAdapter<MovieDetails> {
+    static final String LOG_TAG = GridViewAdapter.class.getSimpleName();
+    Context context;
+    int layoutResourceId;
+    ArrayList<MovieDetails> data = new ArrayList<MovieDetails>();
+
+    public GridViewAdapter(Context context, int layoutResourseId, ArrayList data){
+        super(context, layoutResourseId, data);
+        this.context = context;
+        this.layoutResourceId = layoutResourseId;
+        this.data = data;
+    }
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static boolean isAirplaneModeOn(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return Settings.System.getInt(context.getContentResolver(),
+                    Settings.System.AIRPLANE_MODE_ON, 0) != 0;
+        } else {
+            return Settings.Global.getInt(context.getContentResolver(),
+                    Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+        }
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = null;
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(layoutResourceId, parent, false);
+        if(convertView==null){
+            imageView = new ImageView(context);
+            convertView = imageView;
+        }
+        else{
+            imageView = (ImageView) convertView;
+        }
+
+        final String IMAGE_BASE = "http://image.tmdb.org/t/p/";
+        final String IMAGE_SIZE = "w342";
+        final String IMAGE_URL = data.get(position).getPoster_path();
+        Log.v(LOG_TAG, "Image Url"+IMAGE_URL);
+        String url = IMAGE_BASE+IMAGE_SIZE+IMAGE_URL;
+        Log.v(LOG_TAG, "url1"+url);
+        if(!isAirplaneModeOn(context)){
+            Picasso.with(context).load(url).into(imageView, new Callback() {
+                @Override
+                public void onSuccess() {
+                    Log.e(LOG_TAG, "Success");
+                }
+
+                @Override
+                public void onError() {
+                    Log.e(LOG_TAG, "Error");
+                }
+            });
+        }
+
+        return imageView;
+    }
+}
---
+++ app/src/main/java/app/com/example/android/popularmovies/MainActivityFragment.java
@@ -0,0 +1,181 @@
+package app.com.example.android.popularmovies;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+    GridViewAdapter gridViewAdapter;
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        gridViewAdapter = new GridViewAdapter(getActivity(), R.layout.grid_view_items, new ArrayList<MovieDetails>());
+        GridView gridView = (GridView) rootview.findViewById(R.id.grid_view);
+
+        FetchMovieContent fetchMovieContent = new FetchMovieContent();
+        fetchMovieContent.execute();
+        gridView.setAdapter(gridViewAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootview;
+    }
+
+
+    class FetchMovieContent extends AsyncTask<String, Void, ArrayList<MovieDetails>>{
+
+         final String LOG = FetchMovieContent.class.getSimpleName();
+
+        @Override
+        protected ArrayList<MovieDetails> doInBackground(String... params) {
+
+            //for connection
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String movieJson = null;    //will contain movies as json string
+
+            try {
+                final String MOVIE_BASE_URL = "http://api.themoviedb.org/3/movie/";
+                final String SORT_ORDER = "popular";
+                final String APPID_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                        .appendPath(SORT_ORDER)
+                        .appendQueryParameter(APPID_PARAM, BuildConfig.MOVIEDB_API_KEY)
+                        .build();
+
+                URL url = null;
+                try {
+                    url = new URL(builtUri.toString());
+                    Log.v(LOG, url.toString());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+
+                //Read Input Stream
+                InputStream is = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(is==null){
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while((line=reader.readLine())!=null){
+                    buffer.append(line+"\n");           //for making debugging easy we add newline
+                }
+                if(buffer.length()==0){
+                    //Stream is empty
+                    return null;
+                }
+
+                movieJson = buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }finally {
+                if(urlConnection!=null){
+                    urlConnection.disconnect();
+                }
+                if(reader!=null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                return getMovieData(movieJson);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public ArrayList<MovieDetails> getMovieData(String movieJson) throws JSONException {
+            final String RESULTS = "results";
+            final String ORIGINAL_TITLE = "original_title";
+            final String POSTER_PATH = "poster_path";
+            final String OVERVIEW = "overview";
+            final String VOTE_AVERAGE = "vote_average";
+            final String RELEASE_DATE = "release_date";
+
+
+            JSONObject movies = new JSONObject(movieJson);
+            JSONArray resultsArr = movies.getJSONArray(RESULTS);
+
+            ArrayList<MovieDetails> movieArr = new ArrayList<MovieDetails>();
+
+            for(int i = 0; i < resultsArr.length(); i++){
+                JSONObject eachMovie = resultsArr.getJSONObject(i);
+                MovieDetails mDetails = new MovieDetails(
+                        eachMovie.getString(ORIGINAL_TITLE),
+                        eachMovie.getString(POSTER_PATH),
+                        eachMovie.getString(OVERVIEW),
+                        eachMovie.getString(VOTE_AVERAGE),
+                        eachMovie.getString(RELEASE_DATE)
+                );
+                Log.v(LOG,mDetails.getPoster_path());
+                movieArr.add(mDetails);
+
+            }
+
+            return movieArr;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<MovieDetails> movieDetailses) {
+            if(movieDetailses!=null){
+                gridViewAdapter.clear();
+                for(MovieDetails md : movieDetailses){
+                    gridViewAdapter.add(md);
+                }
+            }
+            super.onPostExecute(movieDetailses);
+        }
+    }
+
+
+}
+
